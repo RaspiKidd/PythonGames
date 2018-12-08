06bed39bcea622c78d50b29246cc4419d3b51036
kerry Kidd <kerry@raspikidd.com>
2018-12-08 14:13:21 +0000

    initial commit for PythonGames

---
+++ gameOver.py
@@ -0,0 +1,7 @@
+# Simple Game Over program
+
+# Demonstrating how print functions work
+
+print("Game Over!") # Prints Game Over! to the screen
+
+input("\n\nPress the enter key to exit.") # prompts the user to "Press the enter key" to exit
