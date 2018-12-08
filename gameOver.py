be5634f7d71467720cc72e751af410a2f7581977
kerry Kidd <kerry@raspikidd.com>
2018-12-08 15:13:13 +0000

    gameOver code v1.0

--- gameOver.py
+++ gameOver.py
@@ -1,7 +1,22 @@
-# Simple Game Over program
+# Simple Game Over program 
+
+# v1.0
 
 # Demonstrating how print functions work
 
-print("Game Over!") # Prints Game Over! to the screen
+print("Program 'Game Over' 1.0") 
+
+# Prints  Game Over! to the screen in ASCII Art
+
+print(
+    """
+       _____          __  __ ______    ______      ________ _____  _            
+     / ____|   /\   |  \/  |  ____|  / __ \ \    / /  ____|  __ \| |           
+    | |  __   /  \  | \  / | |__    | |  | \ \  / /| |__  | |__) | |           
+    | | |_ | / /\ \ | |\/| |  __|   | |  | |\ \/ / |  __| |  _  /| |           
+    | |__| |/ ____ \| |  | | |____  | |__| | \  /  | |____| | \ \|_|           
+     \_____/_/    \_\_|  |_|______|  \____/   \/   |______|_|  \_(_) 
+    """
+)
 
 input("\n\nPress the enter key to exit.") # prompts the user to "Press the enter key" to exit
