789e9737728287ee5bd7287d18c6e13769bf95ee
kerry Kidd <kerry@raspikidd.com>
2018-12-08 15:30:59 +0000

    Fancy Credits v0.0

---
+++ fancyCredits.py
@@ -0,0 +1,23 @@
+# Creating Game Credits
+
+# v 0.0
+
+# A Python program introducing escape chracters
+
+print("\t\t\tFancy Credits")
+
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+
+print("\t\t\tby")
+
+print("\t\t\tRaspiKidd")
+
+print("\t\t\t \\ \\ \\ \\ \\")
+
+print("\nSpecial thanks goes out to:")
+
+# sound the system bell
+
+print("\a")
+
+input("\n\nPress the enter key to exit.")
